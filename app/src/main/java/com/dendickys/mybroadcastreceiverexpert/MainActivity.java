93299d805ccb86b4237ed61f4bfc65225a0b1718
dendickys <febri.unquad2014@gmail.com>
2019-10-02 02:26:54 +0700

    Add SMS receiver, masih ada yg error, blm bisa menampilkan dialog sms

--- app/src/main/java/com/dendickys/mybroadcastreceiverexpert/MainActivity.java
+++ app/src/main/java/com/dendickys/mybroadcastreceiverexpert/MainActivity.java
@@ -1,14 +1,55 @@
 package com.dendickys.mybroadcastreceiverexpert;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.Manifest;
+import android.app.Activity;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button btnCheckPermission;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnCheckPermission = findViewById(R.id.btn_permission);
+        btnCheckPermission.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.btn_permission) {
+            PermissionManager.check(this, Manifest.permission.RECEIVE_SMS, SMS_REQUEST_CODE);
+        }
+    }
+
+    final int SMS_REQUEST_CODE = 101;
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == SMS_REQUEST_CODE) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this, "Sms receiver permission diterima", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(this, "Sms receiver permission ditolak", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    public static class PermissionManager {
+        public static void check(Activity activity, String permission, int requestCode) {
+            if (ActivityCompat.checkSelfPermission(activity, permission) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(activity, new String[]{permission}, requestCode);
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/dendickys/mybroadcastreceiverexpert/SmsReceiverActivity.java
@@ -0,0 +1,47 @@
+package com.dendickys.mybroadcastreceiverexpert;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.app.Activity;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class SmsReceiverActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private TextView tvSmsFrom;
+    private TextView tvSmsMessage;
+    private Button btnClose;
+
+    public static final String EXTRA_SMS_NO = "extra_sms_no";
+    public static final String EXTRA_SMS_MESSAGE = "extra_sms_message";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sms_receiver);
+
+        setTitle(getString(R.string.incoming_message));
+
+        tvSmsFrom = findViewById(R.id.tv_from);
+        tvSmsMessage = findViewById(R.id.tv_message);
+        btnClose = findViewById(R.id.btn_permission);
+        btnClose.setOnClickListener(this);
+
+        String senderNo = getIntent().getStringExtra(EXTRA_SMS_NO);
+        String senderMessage = getIntent().getStringExtra(EXTRA_SMS_MESSAGE);
+
+        tvSmsFrom.setText(String.format("form : %s", senderNo));
+        tvSmsMessage.setText(senderMessage);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.btn_permission) {
+            finish();
+        }
+    }
+}
